ca4fc853df87ba47f8511bdc20eee5d88a0a3ee8
Alon Krumer <alon1525@gmail.com>
2022-11-18 10:35:31 +0200

    added coalition header and cpp file and did party.cpp

---
+++ SkeletonFiles/src/JoinPolicy.cpp
@@ -0,0 +1,11 @@
+#include "JoinPolicy.h"
+
+JoinPolicy::JoinPolicy() 
+{
+    // You can change the implementation of the constructor, but not the signature!
+}
+
+class MandatesJoinPolicy : public JoinPolicy 
+{
+    
+}
